ad6cd42335dd091a2734f6cd463bdb7b387e5a40
Harsimran Kanwar <harsimrankanwar@Harsimrans-MacBook-Pro.local>
2024-01-17 12:30:01 -0500

    fixed detect pieces button + added url to legoset object

--- MobileOgelUI/Models/LegoSet.swift
+++ MobileOgelUI/Models/LegoSet.swift
@@ -10,6 +10,7 @@ import Foundation
 struct LegoSet: Identifiable, Hashable, Equatable {
     let id = UUID()
     var setId: Int
+    var setUrl: String
     var setName: String
     var pieceCount: Int
     var piecesMissing: [LegoPiece]?
@@ -26,9 +27,9 @@ struct LegoSet: Identifiable, Hashable, Equatable {
 
 struct LegoSetMockData {
     //this is data that should come from an endpoint (not necessarily formated to be an object)
-    static let allSampleData: [LegoSet] = [LegoSet(setId: 40570, setName: "Halloween Cat & Mouse", pieceCount: 328), LegoSet(setId: 40570, setName: "Halloween Cat & Mouse", pieceCount: 328, piecesMissing: [LegoPiece(imageName: "2x4_black", pieceName: "Brick 2x4", quantity: 4), LegoPiece(imageName: "2x4_black", pieceName: "Brick 2x4", quantity: 2)]), LegoSet(setId: 40570, setName: "Halloween Cat & Mouse", pieceCount: 328, piecesMissing: [LegoPiece(imageName: "2x4_black", pieceName: "Brick 2x4", quantity: 1)])]
+    static let allSampleData: [LegoSet] = [LegoSet(setId: 40570, setUrl: "https://www.lego.com", setName: "Halloween Cat & Mouse", pieceCount: 328), LegoSet(setId: 40570, setUrl: "https://www.lego.com",setName: "Halloween Cat & Mouse", pieceCount: 328, piecesMissing: [LegoPiece(imageName: "2x4_black", pieceName: "Brick 2x4", quantity: 4), LegoPiece(imageName: "2x4_black", pieceName: "Brick 2x4", quantity: 2)]), LegoSet(setId: 40570, setUrl: "https://www.lego.com", setName: "Halloween Cat & Mouse", pieceCount: 328, piecesMissing: [LegoPiece(imageName: "2x4_black", pieceName: "Brick 2x4", quantity: 1)])]
     
-    static let fuzzySampleData: [LegoSet] = [LegoSet(setId: 40570, setName: "Halloween Cat & Mouse", pieceCount: 328, piecesMissing: [LegoPiece(imageName: "2x4_black", pieceName: "Brick 2x4", quantity: 4), LegoPiece(imageName: "2x4_black", pieceName: "Brick 2x4", quantity: 2)]), LegoSet(setId: 40570, setName: "Halloween Cat & Mouse", pieceCount: 328, piecesMissing: [LegoPiece(imageName: "2x4_black", pieceName: "Brick 2x4", quantity: 1)])]
+    static let fuzzySampleData: [LegoSet] = [LegoSet(setId: 40570, setUrl: "https://www.lego.com", setName: "Halloween Cat & Mouse", pieceCount: 328, piecesMissing: [LegoPiece(imageName: "2x4_black", pieceName: "Brick 2x4", quantity: 4), LegoPiece(imageName: "2x4_black", pieceName: "Brick 2x4", quantity: 2)]), LegoSet(setId: 40570, setUrl: "https://www.lego.com", setName: "Halloween Cat & Mouse", pieceCount: 328, piecesMissing: [LegoPiece(imageName: "2x4_black", pieceName: "Brick 2x4", quantity: 1)])]
     
-    static let perfectSampleData: [LegoSet] = [LegoSet(setId: 40570, setName: "Halloween Cat & Mouse", pieceCount: 328)]
+    static let perfectSampleData: [LegoSet] = [LegoSet(setId: 40570, setUrl: "https://www.lego.com", setName: "Halloween Cat & Mouse", pieceCount: 328)]
 }
--- MobileOgelUI/Views/CapturedImageView.swift
+++ MobileOgelUI/Views/CapturedImageView.swift
@@ -32,46 +32,24 @@ struct CapturedImageView: View {
                                 .padding(20)
                         }
                         
-                        
-                        // what we had before - but added tap gesture does not work for some reason
-                        /*
-                         NavButton(destination: PieceInventoryView(), title: "Detect pieces", width: 200, cornerRadius: 10)
-                         .onTapGesture {
-                         isProcessing = true
-                         print("detect pieces button tapped")
-                         cameraViewModel.processCapturedImage()
-                         }
-                         */
-                        
-                        Button(action: {
-                            isProcessing = true
-                            print("detect pieces button tapped")
-                            
-                            // Perform the processing asynchronously
-                            DispatchQueue.global().async {
-                                cameraViewModel.processCapturedImage()
+                        NavButton(destination: PieceInventoryView(), title: "Detect Pieces", width: 200, cornerRadius: 10)
+                            .simultaneousGesture(TapGesture().onEnded{
+                                isProcessing = true
+                                print("detect pieces button tapped")
                                 
-                                // Update the UI on the main thread
-                                DispatchQueue.main.async {
-                                    isProcessing = false
-                                    // Navigate to PieceInventoryView here if needed
-                                }
-                            }
-                        }) {
-                            Text("Detect pieces")
-                                .bold()
-                                .foregroundColor(.black)
-                                .font(.title3)
-                                .padding(EdgeInsets(top: 24, leading: 12, bottom: 24, trailing: 12))
-                                .frame(width: 200)
-                        }
-                        .background(Color(red: 0.859, green: 0.929, blue: 0.702))
-                        .cornerRadius(10)
-                        .padding(20)
+                                // Perform the processing asynchronously
+                                DispatchQueue.global().async {
+                                    cameraViewModel.processCapturedImage()
+                                    // Update the UI on the main thread
+                                    DispatchQueue.main.async {
+                                        isProcessing = false
+                                        // Navigate to PieceInventoryView here if needed
+                                    }
+                                }})
                     }
                 }
             }
         }
-        .ignoresSafeArea()
+        //.ignoresSafeArea() - for now
     }
 }
