eed6ae6ff9f099cf306525ef027ec999a150a462
Harsimran Kanwar <harsimrankanwar@Harsimrans-MacBook-Pro.local>
2024-03-19 11:43:41 -0400

    cleanup

--- MobileOgelUI/ViewModel/LegoSetViewModel.swift
+++ MobileOgelUI/ViewModel/LegoSetViewModel.swift
@@ -27,24 +27,6 @@ import Observation
         ]
     }
     
-    //helper function to convert db sets to LegoSets
-    func convertToLegoSet(from dict: [String: Any]) -> LegoSet? {
-        guard let setId = dict["setId"] as? Int,
-              let setName = dict["setName"] as? String,
-              let pieceCount = dict["pieceCount"] as? Int,
-              let setUrl = dict["setUrl"] as? String else {
-            print("Failed to extract required values from dictionary: \(dict)")
-            return nil
-        }
-
-        return LegoSet(setId: setId,
-                       setName: setName,
-                       pieceCount: pieceCount,
-                       piecesMissing: nil, // Assuming this will be set later
-                       setUrl: setUrl)
-    }
-    
-    
     func perfectMatchingSets(scannedPieces: [LegoPiece]) async -> [LegoSet]{
         perfectSets = await findPerfectMatches(myPieces: scannedPieces)!
         return perfectSets
